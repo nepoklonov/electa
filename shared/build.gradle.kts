313360587ecf5a21638977ad9479947a96216120
EvaOrange73 <evaorange73@gmail.com>
2022-02-18 12:23:31 +0300

    Method.kt and RpcController.kt moved;

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,9 +3,6 @@ plugins {
     kotlin("plugin.serialization")
 }
 
-val kotlinxSerializationVersion = project.property("kotlinx.serialization.version") as String
-val ktorVersion = project.property("ktor.version") as String
-
 kotlin {
     jvm()
     js {
