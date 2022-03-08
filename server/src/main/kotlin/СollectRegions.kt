5deb77f5dd7f6a724d6819c80e2bc864ff191ef0
Ivan Nepoklonov <i.s.nepoklonov@yandex.ru>
2022-03-08 22:04:52 +0300

    Https possibility added into regex

--- server/src/main/kotlin/СollectRegions.kt
+++ server/src/main/kotlin/СollectRegions.kt
@@ -8,11 +8,11 @@ suspend fun collectRegions(client: HttpClient): Set<RegionDto> {
     client.use {
         val feed = it.get<Document>("http://cikrf.ru/izbiratelnym-komissiyam/sites/")
         val regionsSelect = feed.select("div.cec-select")
-        if (regionsSelect.isEmpty()) {
-            error("no regions found")
-        }
+
+        check(regionsSelect.isNotEmpty()) { "no regions found" }
+
         return regionsSelect.select("option").mapNotNull { region ->
-            val regex = "http://(?:www\\.)?(.*)\\.izbirkom\\.ru".toRegex()
+            val regex = "https?://(?:www\\.)?(.*)\\.izbirkom\\.ru".toRegex()
             val link = region.attr("value")
             val codename = regex.find(link)?.groupValues?.get(1) ?: return@mapNotNull null
             RegionDto(
