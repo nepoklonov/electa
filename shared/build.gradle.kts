3a24f5bf8fcfaa8090dba7d0f1075a60e853789a
PKukui <petyakukui@gmail.com>
2022-03-09 13:57:06 +0300

    client api

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -3,9 +3,20 @@ plugins {
     kotlin("plugin.serialization")
 }
 
+val kotlinxSerializationVersion = project.property("kotlinx.serialization.version") as String
+val kotlinxCoroutinesVersion = project.property("kotlinx.coroutines.version") as String
+
 kotlin {
     jvm()
     js {
         browser()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-js:$kotlinxCoroutinesVersion")
+            }
+        }
     }
 }
